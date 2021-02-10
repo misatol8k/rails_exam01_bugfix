e0abc48facc9e481261a50fa17fb216ec225084a
misatol8k <misatolkurihara@gmail.com>
2021-02-10 16:15:18 +0900

    カラムの型を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_065331) do
+ActiveRecord::Schema.define(version: 2021_02_10_065824) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
